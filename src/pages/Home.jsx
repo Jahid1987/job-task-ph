0ceba2b88045093107864c626186568742df965c
Jahid Anwer <jahid.englaa@gmail.com>
2024-08-15 22:17:03 +0600

    add: fetching total amount of products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,18 +9,24 @@ const Home = () => {
 
   const [itemPerPage, setItemPerPage] = useState(6);
   const [currentPage, setCurrentPage] = useState(0);
-  const count = 50;
-
+  const [count, setCount] = useState(0);
+  // const count = 50;
   const pageNumber = Math.ceil(count / itemPerPage);
   const pages = [...Array(pageNumber).keys()];
 
   useEffect(() => {
     (async function () {
       try {
-        const { data } = await axios.get(
+        // fetching total amount of products
+        const { data: totalProducts } = await axios.get(
+          "http://localhost:5000/productcount"
+        );
+        setCount(totalProducts.count);
+        // fetching all products
+        const { data: products } = await axios.get(
           `http://localhost:5000/products?page=${currentPage}&size=${itemPerPage}`
         );
-        setProducts(data);
+        setProducts(products);
       } catch (error) {
         console.error(error);
       } finally {
@@ -34,10 +40,10 @@ const Home = () => {
 
   return (
     <div className="flex flex-col lg:flex-row gap-2 lg:gap-4">
-      {/* filtering side  */}
+      {/* left side: filtering side  */}
       <div>filtering side here</div>
 
-      {/* products side  */}
+      {/* right side: products*/}
       <div>
         {/* all products showing  */}
         <Products products={products} />
